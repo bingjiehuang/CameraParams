d0a393a001bf65dacf13aa138a4fb1ced49888c3
zxtxin <zxtxin@gmail.com>
2014-10-03 11:55:48 +0800

    milestone

---
+++ src/cn/edu/fudan/ee/glasscamera/CameraParams.java
@@ -0,0 +1,18 @@
+package cn.edu.fudan.ee.glasscamera;
+
+import java.io.Serializable;
+
+/**
+ * Created by zxtxin on 2014/10/1.
+ */
+public class CameraParams implements Serializable {
+    public int params1;
+    public int params2;
+
+    public int getParams1() {
+        return params1;
+    }
+    public int getParams2(){
+        return params2;
+    }
+}
